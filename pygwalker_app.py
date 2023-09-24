f6344d7bdcab3ea37f061af2b2f0517a53c1bd7a
tuliogo <89099757+tuliogo@users.noreply.github.com>
2023-09-24 16:25:38 -0300

    Add files via upload

--- pygwalker_app.py
+++ pygwalker_app.py
@@ -1,17 +1,42 @@
-import streamlit as st
-import pandas as pd
-import pygwalker as pyg
+import streamlit as st 
+import streamlit.components.v1 as stc 
+import pandas as pd 
+import pygwalker as pyg 
 
-st.set_page_config(
-    page_title='Biblioteca PygWalker',
-    page_icon=':snake:',
-    layout='wide',
-    initial_sidebar_state='expanded',
-)
+# Page Configuration
+st.set_page_config(page_title="StWalker App",layout="wide")
 
-df = pd.read_excel('CARROS.xlsx')
+# Load Data Fxn
+def load_data(data):
+    return pd.read_csv(data)
 
-st.title('Biblioteca PygWalker')
-st.subheader('Ferramenta de geração de gráfico')
+def main():
+    st.title("Streamlit PyGWalker App")
 
-pyg.walk(df, env='Streamlit', dark='dark')
+    menu = ["Home", "About"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Home":
+        st.subheader("Home")
+        # Form
+        with st.form("upload_form"):
+            data_file = st.file_uploader("Upload a CSV File",type=["csv","txt"])
+            submitted = st.form_submit_button("Submit")
+
+        if submitted:
+            df = load_data(data_file)
+            st.dataframe(df)
+            # Visualize
+            pyg_html = pyg.walk(df,return_html=True)
+            # Render with components
+            stc.html(pyg_html,scrolling=True,height=1000)
+
+        
+        
+    
+    else:
+        st.subheader("About")
+
+    
+if __name__ == "__main__":
+    main()
